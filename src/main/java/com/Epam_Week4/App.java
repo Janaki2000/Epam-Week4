f0f0f8c8805da0bfd058bab6b8dda2809afc6e62
Janakiram <mjanakiram62@gmail.com>
2020-03-21 12:12:55 +0530

    A maven project to calculate simple and compound interest

---
+++ src/main/java/com/Epam_Week4/App.java
@@ -0,0 +1,10 @@
+package com.Epam_Week4;
+
+public class App 
+{
+    public static void main( String[] args )
+    {
+        UserInterface ui=new UserInterface();
+        ui.take_query();
+    }
+}
